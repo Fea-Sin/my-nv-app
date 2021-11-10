7e5416848188389693e57eb3f2216501a9421319
Fea-Sin <1210992614@qq.com>
2021-11-10 13:46:46 +0800

    提交代码

---
+++ package/start-lint/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "comma-dangle": "off",
+  },
+};
---
+++ package/start-lint/src/main.ts
@@ -0,0 +1,10 @@
+const Hello = "hello";
+
+function say(): void {
+  console.log("eslint--->", Hello);
+}
+
+export default {
+  Hello,
+  say,
+};
